d06a910d031f3621738da7950aabbff10520058f
Andrew <Andrewpw133@gmail.com>
2016-09-05 23:47:45 -1000

    a

--- controllers/main.js
+++ controllers/main.js
@@ -8,14 +8,19 @@
     function main($scope) {
         var vm = this;
         
-        var messageText = "Hello Inspector Stewart.  Your birthday present was stolen by a nefarious thief.  We have only recently apprehended the man responsible (which is why you are just now hearing about this several months after your birthday.  Sorry, we can’t all be super awesome spy people.  Quit judging!).  Unfortunately, the thief quickly sold the item on the black market.  Seeing as it took us this long just to find the first clue, we have decided to leave it up to you, our top spy, to gather more clues and find the whereabouts of the missing package.  Return to this website and report any intel you have gathered so that we can find its final location.  The man who originally stole the package said that he met the buyer in a location that was round and square; hot and cold; wet and dry.";
+        var messageText = "Hello Inspector Stewart._ Your birthday present was stolen by a nefarious thief._We have only recently apprehended the man responsible \n(which is why you are just now hearing about this several months after your birthday. \nSorry, we can’t all be super awesome spy people. Quit judging!)._Unfortunately, the thief quickly sold the item on the black market._Seeing as it took us this long just to find the first clue,\nwe have decided to leave it up to you, our top spy,\nto gather more clues and find the whereabouts of the missing package._Return to this website and report any intel you have gathered\nso that we can find its final location._The man who originally stole the package said that he met the buyer\nin a location that was: \nround and square; hot and cold; wet and dry.";
         var a = document.getElementById("printed-text");
         
         for (var i = 0; i < messageText.length; i++) {
         	(function(i) {
 	        	setTimeout(function(){ 
-	        			a.innerHTML = messageText.substr(0, i);
-	        	}, 300 * i);
+	        		if (messageText.charAt(i) === '_') {
+	        			a.innerHTML = "";
+	        		}
+	        		else {
+	        			a.innerHTML = a.innerHTML + messageText.charAt(i);
+	        		}
+	        	}, 150 * i);
         	})(i);
         }
     }
